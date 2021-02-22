fb2e075508359e390c8839bfd71cb625a42d0b87
Daniel Lopez <dlopezvelazquez@live.com>
2021-02-22 17:51:59 -0600

    prettier

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,22 +1,21 @@
-import React from 'react'; 
-import {Link} from 'react-router-dom'; 
-import styles from './Nav.module.css';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Nav.module.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function nav() {
-    return (
-         <Navbar className={styles.Navbar} fluid>
+  return (
+    <Navbar className={styles.Navbar} fluid>
       <Navbar.Brand as={Link} to="/" className={styles.Brand}>
         <p> UTDesign Crib </p>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse>
-        <Nav className={styles.links}> 
-        </Nav>
+        <Nav className={styles.links}></Nav>
       </Navbar.Collapse>
-    </Navbar>  
-      ) ;
+    </Navbar>
+  );
 }
 
-export default nav; 
+export default nav;
