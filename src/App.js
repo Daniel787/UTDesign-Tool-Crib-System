fb80eb5a1c2ef6eec7d4bfcc3da46b735318a927
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-02-24 15:58:31 -0600

    added exact routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Nav from './Components/Nav/Nav.js';
+import Home from './Pages/Home'
+import Login from './Pages/Login'
 
 function App() {
   return (
-     <Router>
-        <Switch>
-          <Route path="/">
- 
-          </Route>
-          <Route path="/home"> 
-          <Nav/> 
-          </Route>
-        </Switch>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/home">
+          <Nav />
+          <Home />
+        </Route>
+      </Switch>
     </Router>
-    );
+  );
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Nav from "../Components/Nav/Nav.js";
 
-function home() { 
-    <Nav/> 
+function home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+
 }
 
 export default home; 
