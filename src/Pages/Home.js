ae0af28d7f1dc51dd65f44185434dffe1c9ec559
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-02-24 18:45:18 -0600

    Basic Login

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Nav from "../Components/Nav/Nav.js";
+import styles from "./Home.module.css";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 function home() {
     return (
-        <Nav />
+        <div className={styles.Home}>
+            <Nav />
+            <div className={styles.Body}>
+                <h1>Home Page</h1>
+                <Link to="/"><Button>Logout</Button></Link>
+            </div>
+        </div>
+
+
     )
 
 }
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+import styles from "./Login.module.css";
 
 export default function Login() {
+    const loginData = require('../data/authenticate.json');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false)
+    function handleEmpty() {
+        return (username.length === 0 || password.length === 0)
+    }
+    function handleInvalid() {
+        const logins = loginData.logins
+        if (logins.filter(user => user.username === username).length === 0) {
+            return (true)
+        } else {
+            if (loginData.logins[loginData.logins.map(e => e.username).indexOf(username)].password !== password) {
+                return (true)
+            }
+            else {
+                return (false)
+            }
+        }
+    }
     return (
-        <div>
-            <h1>Login Page</h1>
-            <Button />
+        <div className={styles.Login}>
+            <div>
+                <h1>Login Page</h1>
+            </div>
+            <h3 className={styles.Fail} >{error && 'Invalid Login'}</h3>
+            <Form onSubmit={(event) => event.preventDefault()}>
+                <Form.Group size="lg" controlId="user">
+                    <Form.Label>Login:</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="text"
+                        placeholder="Enter Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
+                </Form.Group>
+                <Form.Group size="lg" controlId="pass">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
+                </Form.Group>
+                <Link to={handleInvalid() ? "" : "/home"} >
+                    <Button variant="primary" type="submit" disabled={handleEmpty()} onClick={() => setError(handleInvalid())}>
+                        Sign In
+                    </Button>
+                </Link>
+            </Form>
         </div>
     )
 }
