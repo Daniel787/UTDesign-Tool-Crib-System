624a4ad1a48e3303cf99913dafd2927caa51beaf
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-03-01 15:33:28 -0600

    added links to pages on home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Nav from "../../Components/Nav/Nav.js";
 import styles from "./Home.module.css";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import Footer from '../../Components/Footer/Footer.js';
 
@@ -11,6 +10,14 @@ function home() {
             <Nav />
             <div className={styles.Body}>
                 <h1>Home Page</h1>
+                <ul>
+                    <li><Link to={"/buy"}>Buy Page</Link></li>
+                    <li><Link to={"/rent"}>Rent Page</Link></li>
+                    <li><Link to={"/holds"}>Holds Page</Link></li>
+                    <li><Link to={"/expenses"}>Expenses Page</Link></li>
+                    <li><Link to={"/inventory"}>Inventory Page</Link></li>
+                </ul>
+
             </div>
             <Footer />
         </div>
