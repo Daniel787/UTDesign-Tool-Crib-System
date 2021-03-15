063b8542471ba5e9bea1bb43a3f60a13a9b6f775
Daniel Lopez <dlopezvelazquez@live.com>
2021-03-15 16:57:02 -0500

    Started axios calls

--- src/Pages/Buy/Buy.js
+++ src/Pages/Buy/Buy.js
@@ -1,18 +1,50 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import styles from "./Buy.module.css";
 
 
 export default function Buy() {
-    const [search, setSearch] = useState('')
-    function quaryItem() {
-        console.log('quary: ' + search)
+    const [search, setSearch]  = (""); 
+    const [info, setInfo] = (null);
+
+    // Function for getting info
+    function loadData() { 
+        // Change to correct url later
+         axios.get('/inventory/')
+            .then(res => {
+                const obj = res.data;
+                setInfo(obj); 
+            })
+        }
+    function sendData(obj) {
+        // Change to correct url later
+        axios.post('/inventory', 
+        {
+            id : obj.id,
+            quantity : obj.quantity
+        })
+        .then((respone) => { 
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(response); 
+        });
+    }
+    useEffect(() => {
+        loadData(); 
+    });
+    function queryItem() {
+        console.log('query: ' + search)
     }
     return (
         <div className={styles.Body}>
             <h1>Buy Page</h1>
             <div>
                 Item: <input type="text" onChange={e => setSearch(e.target.value)}></input>
-                <button onClick={quaryItem}>Enter</button>
+                <button onClick={queryItem}>Enter</button>
+                <ul>
+                   {info.map((current, index) => {<li> {current.name}  {current.quantity}  {current.cost} </li>  }) }  
+                </ul> 
             </div>
             {search}
         </div>
