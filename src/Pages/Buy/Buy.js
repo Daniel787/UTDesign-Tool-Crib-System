76951beb1cea441ae2b80ebd948346d04beae617
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-03-15 15:29:38 -0500

    added inout boxes

--- src/Pages/Buy/Buy.js
+++ src/Pages/Buy/Buy.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Buy.module.css";
 
 
 export default function Buy() {
+    const [search, setSearch] = useState('')
+    const [quantity, setQuantity] = useState(0)
     return (
         <div className={styles.Body}>
             <h1>Buy Page</h1>
+            <div>
+                Item: <input type="text" onChange={e => setSearch(e.target.value)}></input>
+                <button>Enter</button>
+            </div>
+            <div>
+                Quantity: <input type="number" onChange={e => setQuantity(e.target.value)}></input>
+                <button>Enter</button>
+            </div>
         </div>
     )
 }
--- src/Pages/Rent/Rent.js
+++ src/Pages/Rent/Rent.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Rent.module.css";
 
 export default function Rent() {
+    const [search, setSearch] = useState('')
+    const [quantity, setQuantity] = useState(0)
     return (
         <div className={styles.Body}>
             <h1>Rent Page</h1>
+            <div>
+                Item: <input type="text" onChange={e => setSearch(e.target.value)}></input>
+                <button>Enter</button>
+            </div>
+            <div>
+                Quantity: <input type="number" onChange={e => setQuantity(e.target.value)}></input>
+                <button>Enter</button>
+            </div>
         </div>
     )
 }
