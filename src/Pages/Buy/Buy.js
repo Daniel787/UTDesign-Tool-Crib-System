1ee275ff35bf8d17f3c3ff3ee67532f4e90585ba
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-03-15 15:34:57 -0500

    removed quatity

--- src/Pages/Buy/Buy.js
+++ src/Pages/Buy/Buy.js
@@ -5,17 +5,17 @@ import styles from "./Buy.module.css";
 export default function Buy() {
     const [search, setSearch] = useState('')
     const [quantity, setQuantity] = useState(0)
+    function quaryItem() {
+        console.log('quary: ' + search)
+    }
     return (
         <div className={styles.Body}>
             <h1>Buy Page</h1>
             <div>
                 Item: <input type="text" onChange={e => setSearch(e.target.value)}></input>
-                <button>Enter</button>
+                <button onClick={quaryItem}>Enter</button>
             </div>
-            <div>
-                Quantity: <input type="number" onChange={e => setQuantity(e.target.value)}></input>
-                <button>Enter</button>
-            </div>
+            {search}
         </div>
     )
 }
--- src/Pages/Rent/Rent.js
+++ src/Pages/Rent/Rent.js
@@ -4,6 +4,9 @@ import styles from "./Rent.module.css";
 export default function Rent() {
     const [search, setSearch] = useState('')
     const [quantity, setQuantity] = useState(0)
+    function quaryItem() {
+        console.log('quary: ' + search)
+    }
     return (
         <div className={styles.Body}>
             <h1>Rent Page</h1>
@@ -11,10 +14,7 @@ export default function Rent() {
                 Item: <input type="text" onChange={e => setSearch(e.target.value)}></input>
                 <button>Enter</button>
             </div>
-            <div>
-                Quantity: <input type="number" onChange={e => setQuantity(e.target.value)}></input>
-                <button>Enter</button>
-            </div>
+            {search}
         </div>
     )
 }
