cdf356fb9173e75693d6abb8b320370741ebc6f1
Daniel Lopez <dlopezvelazquez@live.com>
2021-03-22 16:05:36 -0500

    moved row for each table to separate component

---
+++ src/Components/Buy_Components/Tables/Row.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { Form } from "react-bootstrap";
+import Confirmation_Modal from "../Modal/Confirmation_Modal";
+
+function DataRow(props) {
+  const [amount, setAmount] = useState(1);
+
+  const onChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const formStyle = {
+    width: "100%",
+  };
+  return (
+    <tr>
+      <td> {props.item.part_id} </td>
+      <td> {props.item.name} </td>
+      <td> {props.item.quantity_available} </td>
+      <td> {props.item.current_cost} </td>
+      <td>
+        <Form>
+          <Form.Control style={formStyle} value={amount} onChange={onChange} />
+        </Form>
+      </td>
+      <td>
+        <Confirmation_Modal
+          item={props.item}
+          addToCart={() => {
+            props.addToCart(props.item, amount);
+          }}
+        />
+      </td>
+    </tr>
+  );
+}
+export default DataRow;
--- src/Components/Buy_Components/Tables/Table.js
+++ src/Components/Buy_Components/Tables/Table.js
@@ -2,36 +2,25 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Table, Button, Form } from "react-bootstrap";
 import Confirmation_Modal from "../Modal/Confirmation_Modal";
+import Row from "./Row";
 
 function DataTable(props) {
-  const [list, setList] = useState([]);
-  const formStyle = {
-    width: "100%",
-  };
+  const [displayList, setDisplayList] = useState([]);
+
   function refreshItem() {
     props.refreshList();
-    setList(() =>
+    setDisplayList(() =>
       props.list.map((item) => {
         return (
-          <tr key={item.part_id}>
-            <td> {item.part_id} </td>
-            <td> {item.name} </td>
-            <td> {item.quantity_available} </td>
-            <td> {item.current_cost} </td>
-            <td>
-              <Form>
-                <Form.Control style={formStyle} placeholder="1" />
-              </Form>
-            </td>
-            <td>
-              <Confirmation_Modal item={item} addToCart={props.addToCart} />
-            </td>
-          </tr>
+          <Row key={item.part_id} item={item} addToCart={props.addToCart} />
         );
       })
     );
   }
 
+  useEffect(() => {
+    refreshItem();
+  }, []);
   return (
     <div>
       <Table responsive hover>
@@ -45,7 +34,7 @@ function DataTable(props) {
             <th> Add To Cart</th>
           </tr>
         </thead>
-        <tbody>{list}</tbody>
+        <tbody>{displayList}</tbody>
       </Table>
       <Button onClick={refreshItem}> Refresh List </Button>
       <Button> Go To Cart</Button>
