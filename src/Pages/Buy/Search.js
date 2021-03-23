b97018e5304eb69c592d039e38c6005bcaef30c9
Daniel Lopez <dlopezvelazquez@live.com>
2021-03-23 14:48:02 -0500

    started to make search on buy look a nicer

--- src/Pages/Buy/Search.js
+++ src/Pages/Buy/Search.js
@@ -1,47 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Axios from "axios";
+import styles from "./Search.module.css";
 
 export default function Search(props) {
-    const [idsearch, setIdsearch] = useState(0);
-    const [namesearch, setNamesearch] = useState("");
-    function search(newurl, request) {
-        Axios.get(props.url + newurl + request).then((response) => {
-            response.data.length > 0 ? props.setList(response.data) : props.refreshList();
-        });
-        setIdsearch(0);
-        setNamesearch("");
-    }
+  const [idsearch, setIdsearch] = useState(0);
+  const [namesearch, setNamesearch] = useState("");
+  function search(newurl, request) {
+    Axios.get(props.url + newurl + request).then((response) => {
+      response.data.length > 0
+        ? props.setList(response.data)
+        : props.refreshList();
+    });
+    setIdsearch(0);
+    setNamesearch("");
+  }
 
-    return (
-        <div>
-            <div>
-                <label>By Part ID:</label>{" "}
-                <input
-                    type="number"
-                    value={idsearch}
-                    onChange={(e) => setIdsearch(parseInt(e.target.value))}
-                ></input>
-                <button
-                    disabled={idsearch === 0 || !idsearch}
-                    onClick={() => search("/searchid?id=", idsearch)}
-                >
-                    Search
-              </button>
-            </div>
-            <div >
-                <label>By Name:</label>{" "}
-                <input
-                    type="text"
-                    value={namesearch}
-                    onChange={(e) => setNamesearch(e.target.value)}
-                ></input>
-                <button
-                    disabled={namesearch.length === 0}
-                    onClick={() => search("/searchname?id=", namesearch)}
-                >
-                    Search
-              </button>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.parent}>
+      <div className={styles.container}>
+        {/*  <label>By Part ID: </label>{" "} */}
+        <input
+          type="number"
+          placeholder="By Part ID"
+          value={idsearch}
+          onChange={(e) => setIdsearch(parseInt(e.target.value))}
+        ></input>
+        <button
+          disabled={idsearch === 0 || !idsearch}
+          onClick={() => search("/searchid?id=", idsearch)}
+        >
+          Search
+        </button>
+      </div>
+      <div className={styles.container}>
+        {/* <label>By Name: </label>{" "} */}
+        <input
+          type="text"
+          placeholder="By Name"
+          value={namesearch}
+          onChange={(e) => setNamesearch(e.target.value)}
+        ></input>
+        <button
+          disabled={namesearch.length === 0}
+          onClick={() => search("/searchname?id=", namesearch)}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
 }
