4df8583d69454b82837a6326d5c7d84b8fb59fed
Daniel <dlopezvelazquez@live.com>
2021-04-20 17:14:50 -0500

    current work

--- src/Pages/Group/Components/Student/StudentTable.js
+++ src/Pages/Group/Components/Student/StudentTable.js
@@ -1,32 +1,46 @@
-import React, {useState} from "react";
+import React, { createElement, useState } from "react";
 import { Table } from "react-bootstrap";
 import Axios from "axios";
 import StudentInfo from "./StudentInfo";
 import GroupSubTable from "./GroupSubTable";
+import CreateStudent from "./CreateStudent";
 
 export default function StudentTable(props) {
   const [showStudentModal, setShowStudentModal] = useState(false);
 
   function showStudent() {
-      setShowStudentModal(true);
+    setShowStudentModal(true);
   }
-  
+
   function turnOffStudent() {
-      setShowStudentModal(false);
+    setShowStudentModal(false);
   }
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <td> Name </td>
-          <td> Net ID </td>
-          <td> UTD ID </td>
-          <td> Email </td>
-          <td> Hold </td>
-          <td> Groups </td> 
-        </tr>
-      </thead>
-        {props.list.map((item, i) => {  return (<StudentInfo key={i} item={item} show={showStudentModal} showOn={showStudent} showOff={turnOffStudent} />); } ) }
-    </Table>
+    <React.Fragment>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <td> Name </td>
+            <td> Net ID </td>
+            <td> UTD ID </td>
+            <td> Email </td>
+            <td> Hold </td>
+            <td> Groups </td>
+          </tr>
+        </thead>
+        {props.list.map((item, i) => {
+          return (
+            <StudentInfo
+              key={i}
+              item={item}
+              show={showStudentModal}
+              showOn={showStudent}
+              showOff={turnOffStudent}
+            />
+          );
+        })}
+      </Table>
+      <CreateStudent />
+    </React.Fragment>
   );
 }
