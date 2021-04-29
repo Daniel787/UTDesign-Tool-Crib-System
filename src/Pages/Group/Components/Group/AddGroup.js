3361f4277ec0cf8b570a42826d0d52841f40d20c
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-04-29 15:46:00 -0500

    addGroups rework

--- src/Pages/Group/Components/Group/AddGroup.js
+++ src/Pages/Group/Components/Group/AddGroup.js
@@ -4,33 +4,11 @@ import Axios from "axios";
 
 export default function AddMember(props) {
   const [show, setShow] = useState(false);
-  const [groupId, setGroupId] = useState(null);
-  const [groupName, setGroupName] = useState(null);
-  const [groupSponsor, setGroupSponsor] = useState(null);
+  const [groupId, setGroupId] = useState("");
+  const [groupName, setGroupName] = useState("");
+  const [groupSponsor, setGroupSponsor] = useState("");
+  const [students, setStudents] = useState([])
 
-  const [netid1, setNetid1] = useState("");
-  const [name1, setName1] = useState("");
-  const [email1, setEmail1] = useState("");
-
-  const [netid2, setNetid2] = useState("");
-  const [name2, setName2] = useState("");
-  const [email2, setEmail2] = useState("");
-
-  const [netid3, setNetid3] = useState("");
-  const [name3, setName3] = useState("");
-  const [email3, setEmail3] = useState("");
-
-  const [netid4, setNetid4] = useState("");
-  const [name4, setName4] = useState("");
-  const [email4, setEmail4] = useState("");
-
-  const [netid5, setNetid5] = useState("");
-  const [name5, setName5] = useState("");
-  const [email5, setEmail5] = useState("");
-
-  const [netid6, setNetid6] = useState("");
-  const [name6, setName6] = useState("");
-  const [email6, setEmail6] = useState("");
 
   const host = process.env.REACT_APP_SERVER_SITE;
   const route = process.env.REACT_APP_GROUP_INSERT_SHEET;
@@ -44,47 +22,27 @@ export default function AddMember(props) {
     window.location.reload();
   }
 
-  function handleSubmit(event) {
-    console.log(url);
-    console.log({
-      groups: [
-        {
-          group_id: groupId,
-          group_name: groupName,
-          group_sponsor: groupSponsor,
-          students: [
-            { name: name1, net_id: netid1, email: email1 },
-            { name: name2, net_id: netid2, email: email2 },
-            { name: name3, net_id: netid3, email: email3 },
-            { name: name4, net_id: netid4, email: email4 },
-            { name: name5, net_id: netid5, email: email5 },
-            { name: name6, net_id: netid6, email: email6 },
-          ],
-        },
-      ],
-    });
-    Axios.post(url, {
-      groups: [
-        {
-          group_id: groupId,
-          group_name: groupName,
-          group_sponsor: groupSponsor,
-          students: [
-            { name: name1, net_id: netid1, email: email1 },
-            { name: name2, net_id: netid2, email: email2 },
-            { name: name3, net_id: netid3, email: email3 },
-            { name: name4, net_id: netid4, email: email4 },
-            { name: name5, net_id: netid5, email: email5 },
-            { name: name6, net_id: netid6, email: email6 },
-          ],
-        },
-      ],
-    }).then((response) => {
-      refresh();
-    });
+  function handleSubmit() {
+    console.log(students)
+    // Axios.post(url, {
+    //   groups: [
+    //     {
+    //       group_id: groupId,
+    //       group_name: groupName,
+    //       group_sponsor: groupSponsor,
+    //       students: students,
+    //     },
+    //   ],
+    // }).then((response) => {
+    //   refresh();
+    // });
   }
 
   function showOff() {
+    setGroupId("")
+    setGroupName("")
+    setGroupSponsor("")
+    setStudents([])
     setShow(false);
   }
 
@@ -92,10 +50,31 @@ export default function AddMember(props) {
     setShow(true);
   }
 
+  function addSudent() {
+    let newList = [...students]
+    const defaultSudent = { net_id: "", name: "", utd_id: "", email: "" }
+    newList.push(defaultSudent)
+    setStudents(newList)
+  }
+
+  function removeStudent(index) {
+    let newList = [...students]
+    newList.splice(index, 1)
+    setStudents(newList)
+  }
+
+
+  function invalid() {
+    if (groupSponsor.length > 0 && groupId > 0 && groupName.length > 0) {
+      return (students.filter(e => { return e.name === "" || e.net_id.length !== 9 || e.email === "" }).length > 0)
+    }
+    return true
+  }
+
   return (
     <React.Fragment>
       <Button onClick={showOn}> Add Group </Button>
-      <Modal show={show}>
+      <Modal show={show} onHide={() => showOff()}>
         <Modal.Header>
           <Modal.Title> Add group </Modal.Title>
         </Modal.Header>
@@ -104,6 +83,9 @@ export default function AddMember(props) {
             <Form.Group>
               <Form.Label>Group ID</Form.Label>
               <Form.Control
+                value={groupId}
+                onFocus={e => e.target.select()}
+                type="number"
                 onChange={(e) => {
                   setGroupId(e.target.value);
                 }}
@@ -112,6 +94,8 @@ export default function AddMember(props) {
             <Form.Group>
               <Form.Label> Group Name </Form.Label>
               <Form.Control
+                onFocus={e => e.target.select()}
+                value={groupName}
                 onChange={(e) => {
                   setGroupName(e.target.value);
                 }}
@@ -120,137 +104,63 @@ export default function AddMember(props) {
             <Form.Group>
               <Form.Label> Group Sponsor </Form.Label>
               <Form.Control
+                value={groupSponsor}
+                onFocus={e => e.target.select()}
                 onChange={(e) => {
                   setGroupSponsor(e.target.value);
                 }}
               />
             </Form.Group>
-            <Form.Group>
-              <Form.Label> Student 1 </Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setName1(e.target.value);
-                }}
-                placeholder="Enter Name"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setNetid1(e.target.value);
-                }}
-                placeholder="Enter Net ID"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setEmail1(e.target.value);
-                }}
-                placeholder="Enter email"
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label> Student 2 </Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setName2(e.target.value);
-                }}
-                placeholder="Enter Name"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setNetid2(e.target.value);
-                }}
-                placeholder="Enter Net ID"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setEmail2(e.target.value);
-                }}
-                placeholder="Enter email"
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label> Student 3 </Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setName3(e.target.value);
-                }}
-                placeholder="Enter Name"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setNetid3(e.target.value);
-                }}
-                placeholder="Enter Net ID"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setEmail3(e.target.value);
-                }}
-                placeholder="Enter email"
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label> Student 4 </Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setName4(e.target.value);
-                }}
-                placeholder="Enter Name"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setNetid4(e.target.value);
-                }}
-                placeholder="Enter Net ID"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setEmail4(e.target.value);
-                }}
-                placeholder="Enter email"
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label> Student 5 </Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setName5(e.target.value);
-                }}
-                placeholder="Enter Name"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setNetid5(e.target.value);
-                }}
-                placeholder="Enter Net ID"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setEmail5(e.target.value);
-                }}
-                placeholder="Enter email"
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label> Student 6 </Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setName6(e.target.value);
-                }}
-                placeholder="Enter Name"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setNetid6(e.target.value);
-                }}
-                placeholder="Enter Net ID"
-              />
-              <Form.Control
-                onChange={(e) => {
-                  setEmail6(e.target.value);
-                }}
-                placeholder="Enter email"
-              />
-            </Form.Group>
+            {students.map((current, index) => {
+              return <Form.Group key={index}>
+                <Form.Label> Student {index + 1}
+                </Form.Label> <Button onClick={() => removeStudent(index)}>Remove</Button>
+                <Form.Control
+                  value={current.name}
+                  onFocus={e => e.target.select()}
+                  onChange={(e) => {
+                    let newList = [...students]
+                    newList[index].name = e.target.value
+                    setStudents(newList);
+                  }}
+                  placeholder="Enter Name"
+                />
+                <Form.Control
+                  value={current.net_id}
+                  onFocus={e => e.target.select()}
+                  onChange={(e) => {
+                    let newList = [...students]
+                    newList[index].net_id = e.target.value
+                    setStudents(newList);
+                  }}
+                  placeholder="Enter Net ID"
+                />
+                <Form.Control
+                  value={current.utd_id}
+                  onFocus={e => e.target.select()}
+                  onChange={(e) => {
+                    let newList = [...students]
+                    newList[index].utd_id = e.target.value
+                    setStudents(newList);
+                  }}
+                  placeholder="Enter UTD ID"
+                />
+                <Form.Control
+                  value={current.email}
+                  onFocus={e => e.target.select()}
+                  onChange={(e) => {
+                    let newList = [...students]
+                    newList[index].email = e.target.value
+                    setStudents(newList);
+                  }}
+                  placeholder="Enter email"
+                />
+
+              </Form.Group>
+
+            })}
+            <Button onClick={() => addSudent()}>Add Student</Button>
+
           </Form>
         </Modal.Body>
         <Modal.Footer>
@@ -260,6 +170,7 @@ export default function AddMember(props) {
           </Button>
           <Button
             variant="primary"
+            disabled={invalid()}
             onClick={() => {
               handleSubmit();
               showOff();
