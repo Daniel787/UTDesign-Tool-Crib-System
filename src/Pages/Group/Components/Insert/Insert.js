532a56687a178a046662d1f47b46f73fefa729fb
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-04-30 17:31:24 -0500

    Insertion works assuming sheet is correct

--- src/Pages/Group/Components/Insert/Insert.js
+++ src/Pages/Group/Components/Insert/Insert.js
@@ -5,12 +5,19 @@ import InsertModal from './InsertModal'
 export default function Insert(props) {
     const [status, setStatus] = useState(null)
     const [show, setShow] = useState(false)
+    const host = process.env.REACT_APP_SERVER_SITE;
+    const route = process.env.REACT_APP_GROUP_INSERT_SHEET;
+    // CREATE_REACT_APP was including strings around the route for some reason. Needs to work
+    const modifiedRoute = route.replace(/^"(.*)"$/, "$1");
+    const url = host + modifiedRoute;
 
     function addGroups(sheet) {
-        axios.post(props.url + "/upload", sheet).then(response => {
+        console.log(url)
+        console.log(sheet)
+        axios.post(url, sheet).then(response => {
             if (response.data.message === "SUCCESS") {
                 setStatus(null)
-                // window.location.reload()
+                window.location.reload()
             }
             else {
                 setStatus(response.data)
