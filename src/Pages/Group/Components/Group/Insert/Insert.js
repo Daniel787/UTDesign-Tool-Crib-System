8ca28d067a1515c370e01952596116cf49977316
Daniel Lopez <dlopezvelazquez@live.com>
2021-05-04 18:02:18 -0500

    fixed styling in group using React.Fragment

--- src/Pages/Group/Components/Group/Insert/Insert.js
+++ src/Pages/Group/Components/Group/Insert/Insert.js
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
-import InsertModal from './InsertModal'
-import Error from './ErrorModal/Error'
+import InsertModal from "./InsertModal";
+import Error from "./ErrorModal/Error";
 
 export default function Insert(props) {
+  const [show, setShow] = useState(false);
+  const host = process.env.REACT_APP_SERVER_SITE;
+  const route = process.env.REACT_APP_GROUP_INSERT_SHEET;
+  // CREATE_REACT_APP was including strings around the route for some reason. Needs to work
+  const modifiedRoute = route.replace(/^"(.*)"$/, "$1");
+  const url = host + modifiedRoute;
 
-    const [show, setShow] = useState(false)
-    const host = process.env.REACT_APP_SERVER_SITE;
-    const route = process.env.REACT_APP_GROUP_INSERT_SHEET;
-    // CREATE_REACT_APP was including strings around the route for some reason. Needs to work
-    const modifiedRoute = route.replace(/^"(.*)"$/, "$1");
-    const url = host + modifiedRoute;
-
-    function addGroups(sheet) {
-        console.log(sheet)
-        axios.post(url, sheet).then(response => {
-            if (response.data.message === "SUCCESS") {
-                props.setStatus(null)
-                window.location.reload()
-            }
-            else {
-                console.log(response.data)
-                props.setStatus(response.data)
-            }
-        });
-    }
-    return (
-        <div>
-            <Error status={props.status} setStatus={props.setStatus} addGroups={addGroups} />
-            <InsertModal addGroups={addGroups} show={show} setShow={setShow} />
-        </div>
-    )
+  function addGroups(sheet) {
+    console.log(sheet);
+    axios.post(url, sheet).then((response) => {
+      if (response.data.message === "SUCCESS") {
+        props.setStatus(null);
+        window.location.reload();
+      } else {
+        console.log(response.data);
+        props.setStatus(response.data);
+      }
+    });
+  }
+  return (
+    <React.Fragment>
+      <Error
+        status={props.status}
+        setStatus={props.setStatus}
+        addGroups={addGroups}
+      />
+      <InsertModal addGroups={addGroups} show={show} setShow={setShow} />
+    </React.Fragment>
+  );
 }
--- src/Pages/Group/Components/Group/Insert/InsertModal.js
+++ src/Pages/Group/Components/Group/Insert/InsertModal.js
@@ -3,105 +3,116 @@ import { Modal, Button } from "react-bootstrap";
 import * as XLSX from "xlsx";
 
 export default function InsertModal(props) {
-    const [file, setFile] = useState(null);
-    const [show, setShow] = useState(false)
+  const [file, setFile] = useState(null);
+  const [show, setShow] = useState(false);
 
-    // opens file
-    function readFile() {
-        const reader = new FileReader();
-        reader.onload = (evt) => {
-            /* Parse data */
-            const bstr = evt.target.result;
-            const wb = XLSX.read(bstr, { type: "binary" });
-            /* Get first worksheet */
-            const wsname = wb.SheetNames[0];
-            const ws = wb.Sheets[wsname];
-            /* Convert array of arrays */
-            const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-            /* Update state */
-            props.addGroups({ groups: convertToJson(data) });
-        };
-        reader.readAsBinaryString(file);
-    }
+  // opens file
+  function readFile() {
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      /* Parse data */
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: "binary" });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      /* Update state */
+      props.addGroups({ groups: convertToJson(data) });
+    };
+    reader.readAsBinaryString(file);
+  }
 
-    // converts file to json
-    function convertToJson(csv) {
-        // skips empty lines
-        var lines = csv.split("\n").filter((el) => el);
-        var result = [];
-        var groupHeaders = ["group_id", "group_name", "group_sponsor", "students"];
-        var studentHeader = ["name", "net_id", "email"]
-        for (var i = 1; i < lines.length; i++) {
-            var groupObj = {};
-            var currentline = lines[i].split(",");
-            if (currentline.filter(element => element.length === 0).length !== currentline.length) {
-                groupObj[groupHeaders[0]] = currentline[0];
-                groupObj[groupHeaders[1]] = currentline[3];
-                groupObj[groupHeaders[2]] = currentline[4];
-                groupObj[groupHeaders[3]] = []
-                for (var j = 0; j < parseInt((currentline.length - 6) / 3); j++) {
-                    var studentObj = {}
-                    studentObj[studentHeader[0]] = currentline[6 + j * 3];
-                    studentObj[studentHeader[1]] = currentline[7 + j * 3];
-                    studentObj[studentHeader[2]] = currentline[8 + j * 3];
-                    groupObj[groupHeaders[3]].push(studentObj)
-                }
-                result.push(groupObj);
-            }
+  // converts file to json
+  function convertToJson(csv) {
+    // skips empty lines
+    var lines = csv.split("\n").filter((el) => el);
+    var result = [];
+    var groupHeaders = ["group_id", "group_name", "group_sponsor", "students"];
+    var studentHeader = ["name", "net_id", "email"];
+    for (var i = 1; i < lines.length; i++) {
+      var groupObj = {};
+      var currentline = lines[i].split(",");
+      if (
+        currentline.filter((element) => element.length === 0).length !==
+        currentline.length
+      ) {
+        groupObj[groupHeaders[0]] = currentline[0];
+        groupObj[groupHeaders[1]] = currentline[3];
+        groupObj[groupHeaders[2]] = currentline[4];
+        groupObj[groupHeaders[3]] = [];
+        for (var j = 0; j < parseInt((currentline.length - 6) / 3); j++) {
+          var studentObj = {};
+          studentObj[studentHeader[0]] = currentline[6 + j * 3];
+          studentObj[studentHeader[1]] = currentline[7 + j * 3];
+          studentObj[studentHeader[2]] = currentline[8 + j * 3];
+          groupObj[groupHeaders[3]].push(studentObj);
         }
-        return (result); //JSON
+        result.push(groupObj);
+      }
     }
+    return result; //JSON
+  }
 
-    return (
-        <div>
+  return (
+    <React.Fragment>
+      <Button
+        onClick={() => {
+          setShow(true);
+        }}
+        style={{ marginLeft: "2%" }}
+      >
+        Insert Sheet
+      </Button>
 
-            <Button onClick={() => { setShow(true) }}>
-                Insert Sheet
-            </Button>
-
-            {/* modal */}
-            <Modal
-                show={show}
-                onHide={() => {
-                    setShow(false);
-                    setFile(null)
-                }}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title> Insert Sheet </Modal.Title>
-                </Modal.Header>
-                {/* main part */}
-                <Modal.Body>
-                    <h3>Import a sheet</h3>
-                    {/* prompts user for excel sheet */}
-                    <input type="file" accept=".csv,.xls,.xlsx" onChange={(e) => setFile(e.target.files[0])} />
-                </Modal.Body>
-                {/* options */}
-                <Modal.Footer>
-                    <Button
-                        variant="secondary"
-                        onClick={() => {
-                            setShow(false);
-                            setFile(null)
-                        }}
-                    >
-                        {" "}
-              Cancel{" "}
-                    </Button>
-                    {/* processes file if its valid */}
-                    <Button
-                        variant="primary"
-                        disabled={!file}
-                        onClick={() => {
-                            setShow(false);
-                            readFile();
-                            setFile(null)
-                        }}
-                    >
-                        Confirm
-            </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    );
+      {/* modal */}
+      <Modal
+        show={show}
+        onHide={() => {
+          setShow(false);
+          setFile(null);
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title> Insert Sheet </Modal.Title>
+        </Modal.Header>
+        {/* main part */}
+        <Modal.Body>
+          <h3>Import a sheet</h3>
+          {/* prompts user for excel sheet */}
+          <input
+            type="file"
+            accept=".csv,.xls,.xlsx"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+        </Modal.Body>
+        {/* options */}
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              setShow(false);
+              setFile(null);
+            }}
+          >
+            {" "}
+            Cancel{" "}
+          </Button>
+          {/* processes file if its valid */}
+          <Button
+            variant="primary"
+            disabled={!file}
+            onClick={() => {
+              setShow(false);
+              readFile();
+              setFile(null);
+            }}
+          >
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </React.Fragment>
+  );
 }
